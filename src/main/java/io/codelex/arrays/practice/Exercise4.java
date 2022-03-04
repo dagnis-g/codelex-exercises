637d50e2ac34cbac98269eba6050f00f399903d3
Dagnis Gulbis <dagnisgulbis1@gmail.com>
2022-03-04 11:33:58 +0200

    array exercises

--- src/main/java/io/codelex/arrays/practice/Exercise4.java
+++ src/main/java/io/codelex/arrays/practice/Exercise4.java
@@ -1,5 +1,9 @@
 package io.codelex.arrays.practice;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class Exercise4 {
 
     //TODO: Write a Java program to test if an array contains a specific value.
@@ -7,15 +11,17 @@ public class Exercise4 {
         int[] myArray = {
                 1789, 2035, 1899, 1456, 2013,
                 1458, 2458, 1254, 1472, 2365,
-                1456, 2265, 1457, 2456
+                1456, 2265, 1457, 2456,
         };
 
-        /*
-        fixme - check if contains number 1245 
-        for (?) {
-            ......
-            System.out.println("Contains!");
+
+        // fixme - check if contains number 1245
+        int num = 1245;
+        if (Arrays.stream(myArray).anyMatch(i -> i == num)) {
+            System.out.println("Contains number: " + num);
+        } else {
+            System.out.println("Doesn't contain number: " + num);
         }
-        */
+
     }
 }
--- src/main/java/io/codelex/arrays/practice/Exercise5.java
+++ src/main/java/io/codelex/arrays/practice/Exercise5.java
@@ -1,29 +1,40 @@
 package io.codelex.arrays.practice;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Exercise5 {
 
     //TODO: Write a Java program to find the index of an array element.
     public static void main(String[] args) {
         int[] myArray = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
 
-        int indexOfElement36 = -1;
-        int indexOfElement29 = -1;
+        int indexOfElement36 = 0;
+        int indexOfElement29 = 0;
 
-        /*
-        fixme
-        for (?) {
-            
+
+        //fixme
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] == 36) {
+                indexOfElement36 = i;
+            }
+            if (myArray[i] == 29) {
+                indexOfElement29 = i;
+            }
         }
-        */
 
+//        Integer[] myArray1 = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+//        int indexOfElement25 = List.of(myArray1).indexOf(25);
+//        System.out.println("Index position of 25 is: " + indexOfElement25);
+        
         //Expected output:
         //Index position of 36 is: 4
         //Index position of 29 is: 8
-        
-        /*
-        fixme
+
+
+        //fixme
         System.out.println("Index position of 36 is: " + indexOfElement36);
         System.out.println("Index position of 29 is: " + indexOfElement29);
-        */
+
     }
 }
