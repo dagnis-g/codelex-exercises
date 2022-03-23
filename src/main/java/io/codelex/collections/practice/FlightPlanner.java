de5a28c3793db0994d997de8c468a3a1ee0b2639
Dagnis Gulbis <dagnisgulbis1@gmail.com>
2022-03-23 10:09:01 +0200

    collectionsTextFiles

--- src/main/java/io/codelex/collections/practice/FlightPlanner.java
+++ src/main/java/io/codelex/collections/practice/FlightPlanner.java
@@ -6,13 +6,14 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class FlightPlanner {
     private static final Charset charset = Charset.defaultCharset();
     private static final String file = "/collections/flights.txt";
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        final Path path = Paths.get(Histogram.class.getResource(file).toURI());
+        final Path path = Paths.get(Objects.requireNonNull(Histogram.class.getResource(file)).toURI());
         Files.readAllLines(path, charset);
     }
 }
--- src/main/java/io/codelex/collections/practice/WordCount.java
+++ src/main/java/io/codelex/collections/practice/WordCount.java
@@ -6,13 +6,30 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
 
 public class WordCount {
     private static final Charset charset = Charset.defaultCharset();
     private static final String file = "/collections/lear.txt";
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        final Path path = Paths.get(Histogram.class.getResource(file).toURI());
-        Files.readAllLines(path, charset);
+        final Path path = Paths.get(Objects.requireNonNull(Histogram.class.getResource(file)).toURI());
+
+        List<String> lines = Files.readAllLines(path, charset);
+        System.out.println("Words = " + lines.size());
+
+        String[] textString = String.valueOf(Files.readAllLines(path, charset)).split("\\s+");
+        System.out.println("Words = " + textString.length);
+
+        char[] textChars = String.valueOf(Files.readAllLines(path, charset)).toCharArray();
+        int charCount = 0;
+        for (char i : textChars) {
+            if (Character.isLetterOrDigit(i)) {
+                charCount++;
+            }
+        }
+        System.out.println("Chars = " + charCount);
+
     }
 }
