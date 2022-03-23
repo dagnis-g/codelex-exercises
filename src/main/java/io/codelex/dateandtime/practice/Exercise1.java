b23e093414ea40ea275ced9b63bcc58c874c99b0
Dagnis Gulbis <dagnisgulbis1@gmail.com>
2022-03-23 10:28:48 +0200

    fixes

--- src/main/java/io/codelex/dateandtime/practice/Exercise1.java
+++ src/main/java/io/codelex/dateandtime/practice/Exercise1.java
@@ -1,13 +1,15 @@
 package io.codelex.dateandtime.practice;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Exercise_1 {
+public class Exercise1 {
 
     public static void main(String[] args) {
+        int WORKING_HOURS_IN_DAY = 8;
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter start date(example: 01-01-2022)");
@@ -22,18 +24,24 @@ public class Exercise_1 {
         int workingDayCount = 0;
 
         for (int i = 0; i <= dayCountBetweenPeriods; i++) {
-            String today = String.valueOf(startDate.plusDays(i).getDayOfWeek());
-            if (!(today.equalsIgnoreCase("Saturday") || today.equalsIgnoreCase("Sunday"))) {
+            DayOfWeek today = startDate.plusDays(i).getDayOfWeek();
+            if (isWorkDay(today)) {
                 workingDayCount++;
             }
         }
 
-        int hoursWorked = workingDayCount * 8;
+        int hoursWorked = workingDayCount * WORKING_HOURS_IN_DAY;
         System.out.println("During this time period, employee worked for " + hoursWorked + " hours");
     }
 
+    private static boolean isWorkDay(DayOfWeek today) {
+        return !(today.equals(DayOfWeek.SATURDAY) || today.equals(DayOfWeek.SUNDAY));
+    }
+
     private static LocalDate inputDateToLocalDate(String date) {
-        int[] splitDate = Arrays.stream(date.split("-")).mapToInt(Integer::parseInt).toArray();
+        int[] splitDate = Arrays.stream(date.split("-"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
         return LocalDate.of(splitDate[2], splitDate[1], splitDate[0]);
     }
 
